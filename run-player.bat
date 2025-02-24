e1d90804a753f6f5be24b84f0a8d34b6ea7be134
xayanide <xayanide@gmail.com>
2025-02-25 00:14:23 +0800

    refactor: improve batch files

--- run-player.bat
+++ run-player.bat
@@ -1,2 +1,4 @@
-cd /d "%~dp0"
-node index.js player "%*"
+@echo off
+set "args=player"
+if not "%~1"=="" set "args=player "%*""
+node "%~dp0index.js" %args%
--- run-studio.bat
+++ run-studio.bat
@@ -1,2 +1,4 @@
-cd /d "%~dp0"
-node index.js studio "%*"
+@echo off
+set "args=studio"
+if not "%~1"=="" set "args=studio "%*""
+node "%~dp0index.js" %args%
