815b55e62c9149521db80217c44c5970c063afe5
xayanide <xayanide@gmail.com>
2025-10-25 09:53:07 +0800

    refactor: use winston

--- modules/logger.js
+++ modules/logger.js
@@ -1,10 +1,16 @@
 import * as nodePath from "node:path";
 import * as nodeFsPromises from "node:fs/promises";
 import * as nodeProcess from "node:process";
-import SimpleLogger from "./loggerClass.js";
+import * as winston from "winston";
 import { getDirname } from "./fileUtils.js";
 import { APP_TYPES, BINARY_TYPES_MAP } from "./constants.js";
 
+const { combine, printf, colorize, timestamp: winstonTimestamp } = winston.format;
+const utcTimestamp = winstonTimestamp({
+    format: function () {
+        return new Date().toISOString();
+    },
+});
 const logFileISOTimestamp = new Date().toISOString().split(":").join("-");
 const metaUrl = import.meta.url;
 
@@ -31,8 +37,25 @@ const appType = argv.find((arg) => {
 });
 const binaryType = BINARY_TYPES_MAP[appType];
 
+function printLog({ level, message, timestamp }) {
+    return `${timestamp} [${level}]  ${binaryType}: ${message}`;
+}
 const logsFilePath = nodePath.join(getDirname(metaUrl), "..", "logs", `${binaryType ?? "unknown"}-${logFileISOTimestamp}.log`);
 await deleteOldLogFiles(nodePath.dirname(logsFilePath));
-const logger = SimpleLogger.createLogger(binaryType ?? "Unknown", { filePath: logsFilePath, appendFile: true });
-
+const logger = winston.createLogger({
+    level: "debug",
+    format: combine(utcTimestamp, printf(printLog)),
+    transports: [
+        new winston.transports.Console({
+            format: combine(utcTimestamp, colorize(), printf(printLog)),
+        }),
+        new winston.transports.File({
+            filename: logsFilePath,
+            // 5MB per file
+            maxsize: 5 * 1024 * 1024,
+            // optional, keep last 5 files
+            maxFiles: 5,
+        }),
+    ],
+});
 export default logger;
