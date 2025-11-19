407381dc713d242f5c7f839ab8b6f2d2501ba2e5
xayanide <xayanide@gmail.com>
2025-11-20 01:39:57 +0800

    fix(extractZip): studio map type incorrect

--- modules/extractZip.js
+++ modules/extractZip.js
@@ -7,30 +7,30 @@ function getMapType(fileName, folderMappings) {
     if (playerOnly && fileName in playerOnly) {
         return "_playerOnly";
     }
-    const studioOnly = folderMappings._playerOnly;
+    const studioOnly = folderMappings._studioOnly;
     if (studioOnly && fileName in studioOnly) {
         return "_studioOnly";
     }
     return "_common";
 }
 
-function resolveMappedPath(mapType, fileName, folderMappings) {
+function resolveMappedPath(fileName, folderMappings) {
+    const mapType = getMapType(fileName, folderMappings);
     const mappedPath = folderMappings[mapType][fileName];
     // do not use !mappedPath guard because "" should be accepted
     if (mappedPath === null) {
-        logger.warn(`File '${fileName}' has no mapped path! This file will be extracted at root.`);
+        logger.warn(`Map type: ${mapType}. File '${fileName}' has no mapped path! This file will be extracted at root.`);
         return "";
     }
     if (mappedPath === undefined) {
-        throw new Error(`File '${fileName}' has an undefined mapped path!`);
+        throw new Error(`Map type: ${mapType}. File '${fileName}' has an undefined mapped path!`);
     }
     return mappedPath;
 }
 
 const extractZip = (filePath, extractPath, folderMappings) => {
     const fileName = nodePath.basename(filePath);
-    const mapType = getMapType(fileName, folderMappings);
-    const mappedPath = resolveMappedPath(mapType, fileName, folderMappings);
+    const mappedPath = resolveMappedPath(fileName, folderMappings);
     const targetPath = nodePath.join(extractPath, mappedPath);
     return new Promise(function (resolve, reject) {
         try {
